700b0308b48950fb1ebc1e8f18fbc58f641feb7c
unknown <hochardpierrot@gmail.com>
2022-04-26 22:31:43 +0200

    V0.2

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.swiper1', {
     // Optional parameters
     direction: 'horizontal',
     loop: false,
 
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next1',
+      prevEl: '.swiper-button-prev1',
     },
 
     pagination: {
-      el: ".swiper-pagination",
+      el: ".swiper-pagination1",
     },
+});
+
+const swiper2 = new Swiper('.weekswiper', {
+  
+  direction: 'horizontal',
+  loop: false,
+  slidesPerView: 1.5,
+  spaceBetween: 30,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next2',
+      prevEl: '.swiper-button-prev2',
+    },
+
+    pagination: {
+      el: ".swiper-pagination2",
+    },
+
 });
--- navbar.js
+++ navbar.js
@@ -11,3 +11,20 @@ JERECLICK.onclick = function(){
     LANAV.style.transform="translate(-500px)";
     JECLICK.style.transform="translate(0)";
 }
+
+window.onscroll=function(){
+    var TOPBAR=document.getElementById("topbar");
+
+    if(document.documentElement.scrollTop>80){
+        TOPBAR.style.backgroundColor="white"
+        TOPBAR.style.color="black"
+        TOPBAR.style.textShadow="none"
+    }
+    else{
+        TOPBAR.style.backgroundColor="transparent"
+        TOPBAR.style.color="white"
+        TOPBAR.style.textShadow="2px 2px 20px #000000"
+    }
+
+
+}
