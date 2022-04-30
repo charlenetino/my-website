193bad41846bf0f7150acb6e66e2efde76ea5b45
unknown <hochardpierrot@gmail.com>
2022-04-30 15:43:21 +0200

    V0.4

--- navbar.js
+++ navbar.js
@@ -1,16 +1,25 @@
-/*document.body.onload=function(){
-    var JECLICK=document.getElementById("JECLICK");
-    var JERECLICK=document.getElementById("JERECLICK");
-    var LANAV=document.getElementById("LANAV");
+document.body.onload=function(){
+    var comming=document.getElementById("comming");
+    var soon=document.getElementById("soon");
+    var comming2=document.getElementById("comming2");
+    var soon2=document.getElementById("soon2");
 }
-JECLICK.onclick = function(){
-    LANAV.style.transform="translate(0)";
-    JECLICK.style.transform="translate(-500px)";
+function timereturn() {
+    soon.style.transform="translateY(200px)";
 }
-JERECLICK.onclick = function(){
-    LANAV.style.transform="translate(-500px)";
-    JECLICK.style.transform="translate(0)";
-}*/
+function timereturn2() {
+    soon2.style.transform="translateY(200px)";
+}
+
+comming.onclick = function(){
+    soon.style.transform="translateY(0)";
+    setTimeout(timereturn, 5000);
+}
+
+comming2.onclick = function(){
+    soon2.style.transform="translateY(0)";
+    setTimeout(timereturn2, 5000);
+}
 
 window.onscroll=function(){
     var TOPBAR=document.getElementById("topbar");
